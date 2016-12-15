e8b93232f98e540f5ce1e69b80b9ef4cbdcecb45
Amanda <amanda@Amandas-MBP.fios-router.home>
2016-12-15 16:51:23 -0500

    beautified

--- db/migrate/20161127204138_create_pages.rb
+++ db/migrate/20161127204138_create_pages.rb
@@ -1,10 +1,11 @@
 class CreatePages < ActiveRecord::Migration[5.0]
   def change
     create_table :pages do |t|
+      t.date :date
       t.string :title
       t.string :content
       t.string :mood
-      t.timestamps
+      t.timestamps :timestamp
     end
   end
 end
